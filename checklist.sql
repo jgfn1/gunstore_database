938d984099a8d982ea6117fc2ed537fdc44cd72a
jgfn1 <jgfn@cin.ufpe.br>
2017-10-07 17:12:41 -0300

    Ignoring .sh files and creating the checklist file with the instructions of what needs to be done.

---
+++ checklist.sql
@@ -0,0 +1,125 @@
+/*
+Checklist 1 - SQL e PL - GDI EC 2017.2
+
+Obs.: As questoes feitas devem ser identificadas com identacao de
+duas "tabs" e colocadas entre parenteses.
+  Ex. (A 2. foi feita porem a 1. nao.):
+      1. Faca isto.
+          (2. Faca aquilo.)
+
+Obs. 1: Devemos prestar atenção em qual dos três códigos devemos copiar
+os itens que fazemos do checklist.
+
+Orientações:
+1. Os alunos devem indicar que itens são abordados em cada trecho de código. É possível
+apresentar um só trecho de código que contenha mais de um item.
+2. É necessário que os alunos saibam explicar exatamente o que cada trecho de código
+faz; caso contrário, não será considerada a pontuação do item.
+3. As consultas SQL devem sempre retornar pelo menos uma linha.
+4. Funções e procedimentos PL devem estar acompanhados de trechos de código que os
+executem, para demonstração.
+5. Gatilhos devem estar acompanhados de instruções que os ativem, para demonstração.
+
+SQL - básicas
+1. Uso de BETWEEN com valores numéricos
+2. Uso de BETWEEN com datas
+3. Uso de LIKE/NOT LIKE com tokens (% ou _)
+4. Uso de IN com subconsulta
+5. Uso de IS NULL/IS NOT NULL
+6. Uso de ORDER BY
+7. Criação de VIEW
+8. Consulta sobre VIEW
+9. Deleção de VIEW
+10. Criar CHECKs
+11. Criar PK Composta
+12. Criar FK Composta
+13. Usar Valor DEFAULT (Ex: Data do Sistema)
+14. Usar ALTER TABLE para Modificação de Coluna
+15. Usar ALTER TABLE para Adicionar Coluna
+16. Usar ALTER TABLE para Remover de Coluna
+17. Operadores aritméticos no SELECT
+18. Função de agregação sem GROUP BY
+19. Função de agregação com GROUP BY
+20. Uso de DISTINCT
+21. Uso de HAVING
+22. Uso de HAVING com subconsulta
+23. Uso de WHERE + HAVING
+24. Junção entre duas tabelas
+25. Junção entre três tabelas + condição de seleção (M:N)
+26. Junção usando INNER JOIN
+27. Junção usando LEFT OUTER JOIN
+28. Junção usando RIGHT OUTER JOIN
+29. Junção usando FULL OUTER JOIN
+30. Uma subconsultacom uso de ANY ou SOME
+31. Uma subconsultacom uso de ALL
+32. Uma subconsultacom uso de EXISTS/NOT EXISTS
+33. Uma subconsultacom uso de ALIAS com consultas aninhadas (ALIAS externo sendo
+referenciado na subconsulta)
+34. Uso de UNION
+35. Uso de INTERSECT
+36. Uso de MINUS
+37. INSERT com subconsulta
+38. UPDATE com subconsulta
+39. DELETE com subconsulta
+40. Uso de GRANT
+41. Uso de REVOKE
+
+SQL - avançadas
+42. Subconsulta dentro da cláusula FROM (VIEW implícita)
+43. Operação aritmética com função de agregação como operador
+44. Uso de BETWEEN com valores numéricos retornados por funções de agregação
+45. Junção entre três tabelas usando INNER JOIN ou OUTER JOIN
+46. ORDER BY com mais de dois campos
+47. EXISTS com mais de uma tabela, sem fazer junção
+
+PL - básicas
+48. Bloco anônimo com declaração de variável e instrução
+49. Bloco anônimo com exceção
+50. Uso de IF-THEN-ELSE
+51. Uso de ELSIF
+52. Uso de CASE
+53. LOOP com instrução de saída
+54. WHILE LOOP
+55. FOR LOOP
+56. Recuperação de dados para variável
+57. Recuperação de dados para registro
+58. Output de string com variável
+59. Uso de cursor explícito com variável
+60. Uso de cursor explícito com registro
+61. Uso de cursor explícito com parâmetro
+62. Cursor dentro de FOR (sem DECLARE)
+63. Procedimento sem parâmetro
+64. Procedimento com parâmetro IN
+65. Procedimento com parâmetro OUT
+66. Procedimento com parâmetro INOUT
+67. Uso de procedimento dentro de outro bloco PL (pode-se usar um dos procedimentos
+criados anteriormente)
+68. Função sem parâmetro
+69. Função comparâmetro IN
+70. Função comparâmetro OUT
+71. Função comparâmetro INOUT
+72. Criação de pacote (declaração e corpo) com pelo menos dois componentes
+73. BEFORE TRIGGER
+74. AFTER TRIGGER
+75. TRIGGER de linha sem condição
+76. TRIGGER de linha com condição
+77. TRIGGER de comando
+78. Uso de NEW em TRIGGER de inserção
+79. Uso de OLD em TRIGGER de deleção
+80. Uso de NEW e OLD em TRIGGER de atualização
+81. Uso de TRIGGER para impedir inserção em tabela
+82. Uso de TRIGGER para impedir atualização em tabela
+83. Uso de TRIGGER para impedir deleção em tabela
+84. Uso de TRIGGER para inserir valores em outra tabela
+85. Uso de TRIGGER para atualizar valores em outra tabela
+86. Uso de TRIGGER para apagar valores em outra tabela
+
+PL - avançadas
+87. Uso de função dentro de uma consulta SQL (pode-se usar uma das funções criadas
+anteriormente)
+88. Registro como parâmetro de função ou procedimento
+89. Função com registro como retorno
+90. Pacote com funções ou procedimentos que usem outros componentes do mesmo
+pacote
+91. INSTEAD OF TRIGGER
+*/
