ff9949a7f26d16814f193f0cdfe3382b426a5fe4
jgfn1 <jgfn@cin.ufpe.br>
2017-11-15 14:40:52 -0300

    Template for the OR SQL exercises.

---
+++ or_sql/checklist.sql
@@ -0,0 +1,65 @@
+/*
+Checklist 2 - OR - GDI EC 2017.2
+
+Obs.: As questoes feitas devem ser identificadas com identacao de
+duas "tabs" e dois hífens "--".
+  Ex. (A 2. foi feita porem a 1. nao.):
+      1. Faca isto.
+          --2. Faca aquilo.
+
+Obs. 1: Devemos prestar atenção em qual dos três códigos devemos copiar
+os itens que fazemos do checklist.
+
+Orientações:
+1. Os alunos devem indicar que itens são abordados em cada trecho de código. É possível
+apresentar um só trecho de código que contenha mais de um item.
+2. É necessário que os alunos saibam explicar exatamente o que cada trecho de código faz (não
+podem ser coisas absurdas!); caso contrário, não será considerada a pontuação do item.
+3. As consultas SQL devem sempre retornar pelo menos uma linha.
+4. Código PL (trigger, função ou procedimento) deve estar acompanhado de um trecho de código
+que o execute/teste.
+
+Atividades
+1. Criação de tipo e subtipo
+2. Criação de um tipo que contenha um atributo que seja de um outro tipo
+3. Criação de um tipo que contenha um atributo que seja de um tipo VARRAY
+4. Criação de um tipo que contenha um atributo que seja de um tipo NESTED TABLE
+5. Criação e chamada de um método construtor (diferente do padrão)
+6. Criação e chamada de um função membro em um comando SELECT e em um bloco PL
+7. Criação e chamada de um método MAP em um comando SELECT e em um bloco PL
+8. Criação e chamada de um método ORDER em um comando SELECT e em um bloco PL
+9. Criação e chamada de método abstrato
+10. Redefinição de método do supertipo dentro do subtipo
+11. Alteração de tipo: adição de atributo
+12. Alteração de tipo: modificação de atributo
+13. Alteração de tipo: remoção de atributo
+14. Alteração de supertipo com propagação de mudança
+15. Alteração de supertipo com invalidação de subtipos afetados
+16. Uso de referência e controle de integridade referencial
+17. Restrição de escopo de referência
+18. Criação de todas as tabela a partir de um tipo
+19. Criação de uma consulta com expressão de caminho parapercorrer três tabelas
+20. Criação de uma consulta com DEREF
+21. Criação de uma consulta com VALUE
+22. Criação de uma consulta com TABLE
+23. Criação de consultas com LIKE, BETWEEN, ORDER BY, GROUP BY, HAVING
+24. Criação de subconsultas com IN , ALL, ANY,
+25. Criação de uma consulta que exiba os dados de um VARRAY
+26. Criação de uma consulta que exiba os dados de um NESTED TABLE
+27. SELECT com EXISTS para acessar os dados de uma tabela A com referência para outra
+tabela B, a partir da tabela B
+28. Criação de TRIGGER de linha ao ocorrer um INSERT, DELETEou UPDATE
+29. Criação de TRIGGER de linha para impedir INSERT, DELETEou UPDATE
+30. Criação de TRIGGER de comando para impedir INSERT, DELETEou UPDATE
+*/
+
+0--
+1--
+2
+3--
+4--
+5
+6--
+7--
+8
+9--
