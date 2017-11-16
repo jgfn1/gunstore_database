e221e05ccc0ba61b0138a53e1924571964ce7f55
Paulo Diógenes Silva Salgado Filho <pdssf@cin.ufpe.br>
2017-11-16 08:58:34 -0300

    Creating tp_persons and finshing

--- or_sql/create.sql
+++ or_sql/create.sql
@@ -2,13 +2,10 @@
 CREATE OR REPLACE TYPE tp_adress AS OBJECT(
 	zipcode integer
 	street varchar2(100)
-);
+)FINAL;
 /
-
 CREATE OR REPLACE TYPE tp_phones AS TABLE OF VARCHAR2 (9);
 /
-
-*/
 CREATE OR REPLACE TYPE tp_persons AS OBJECT(
 	cpf INTEGER,
 	name VARCHAR2(100),
@@ -16,5 +13,4 @@ CREATE OR REPLACE TYPE tp_persons AS OBJECT(
 	sex VARCHAR2(1),
 	adress tp_adress,
 	phones tp_phones
-); --final??? instatiable??  not complete
-/
+)NOT FINAL  NOT INSTANCIABLE; -- THERE WILL BE SUBCLASSES OF tp_persons BUT THERE WILL NOT EXISTS A INSTANCE tp_persons, someone is either a employeer or a client
