3f5901122b8b663b431a38b4c830e7160fb2aba5
Paulo Diógenes Silva Salgado Filho <pdssf@cin.ufpe.br>
2017-11-16 16:40:05 -0300

    finishing create, resolved all the issues.

--- or_sql/create.sql
+++ or_sql/create.sql
@@ -1,15 +1,23 @@
+DROP TYPE tp_adress force;
+DROP TYPE tp_phones force;
+DROP TYPE tp_persons force;
+DROP TYPE tp_employees force;
+DROP TYPE tp_clients force;
+DROP TYPE tp_department force;
+DROP TYPE tp_artifact force;
+DROP TYPE tp_overtimes force;
+DROP TYPE tp_vacancies force;
 
 CREATE OR REPLACE TYPE tp_adress AS OBJECT(
 	zipcode integer,
 	street varchar2(100)
 )FINAL;
 /
-
 CREATE OR REPLACE TYPE tp_phones AS TABLE OF VARCHAR2 (9);
 /
 
---2. Criação de um tipo que contenha um atributo que seja de um outro tipo
---4. Criação de um tipo que contenha um atributo que seja de um tipo NESTED TABLE
+--2. CriaÃ§Ã£o de um tipo que contenha um atributo que seja de um outro tipo
+--4. CriaÃ§Ã£o de um tipo que contenha um atributo que seja de um tipo NESTED TABLE
 CREATE OR REPLACE TYPE tp_persons AS OBJECT(
 	cpf INTEGER,
 	name VARCHAR2(100),
@@ -17,24 +25,14 @@ CREATE OR REPLACE TYPE tp_persons AS OBJECT(
 	sex VARCHAR2(1),
 	adress tp_adress,
 	phones tp_phones
-)NOT FINAL  NOT INSTANCIABLE; -- THERE WILL BE SUBCLASSES OF tp_persons BUT THERE WILL NOT EXISTS A INSTANCE tp_persons, someone is either a employeer or a client
+)NOT FINAL  NOT INSTANTIABLE; -- THERE WILL BE SUBCLASSES OF tp_persons BUT THERE WILL NOT EXISTS A INSTANCE tp_persons, someone is either a employeer or a client
+/
 
-/*
-CREATE TABLE departments(
-  department_code INTEGER,
-  name VARCHAR2 (100),
-  phone_extension INTEGER, --In Portuguese it is called "Ramal".
-  manager_cpf INTEGER NOT NULL,
-  CONSTRAINT departments_pkey PRIMARY KEY (department_code)
-);
-*/
-
-
-CREATE OR REPLACE TYPE tp_departments AS OBJECT(
+CREATE OR REPLACE TYPE tp_department AS OBJECT(
 	department_code INTEGER,
  	name VARCHAR2 (100),
  	phone_extension INTEGER, --In Portuguese it is called "Ramal".
- 	manager_cpf REF tp_employees
+ 	manager_cpf REF tp_persons
 )FINAL;
 /
 /*
@@ -52,126 +50,63 @@ CREATE TABLE employees
   CONSTRAINT persons_fkey2 FOREIGN KEY (department_code) REFERENCES departments (department_code)
 );
 */
-    --1. Criação de tipo e subtipo
-CREATE OR REPLACE TYPE tp_employees UNDER tp_persons AS OBJECT(
+    --1. CriaÃ§Ã£o de tipo e subtipo
+CREATE OR REPLACE TYPE tp_employees UNDER tp_persons(
 	wage INTEGER,
   	worked_years INTEGER,
   	job_title VARCHAR2(100),	
-  	ref_supervisor REF tp_empregado,
+  	ref_supervisor REF tp_employees,
   	ref_department_code REF tp_department
 )FINAL ;
 /
-/*
-CREATE TABLE clients
-(
-  client_cpf INTEGER,
-  purchases_number INTEGER,
-  heavy_guns_license BINARY_DOUBLE, -- 1 - Have the License | 0 - Don't.
-  training_end_register BINARY_DOUBLE, -- 1 - Finished the training | 0 - Don't.
-  CONSTRAINT clients_pkey PRIMARY KEY (client_cpf),
-  CONSTRAINT clients_fkey FOREIGN KEY (client_cpf) REFERENCES persons (cpf)
-);
 
-*/
-CREATE OR REPLACE TYPE tp_clients UNDER tp_persons AS OBJECT(
+CREATE OR REPLACE TYPE tp_clients UNDER tp_persons(
 	purchases_number INTEGER,
 	heavy_guns_license BINARY_DOUBLE, -- 1 - Have the License | 0 - Don't.
   	training_end_register BINARY_DOUBLE -- 1 - Finished the training | 0 - Don't.
 )FINAL;
 /
-/*
-CREATE TABLE artifacts
-(
-  artifact_code INTEGER,
-  name VARCHAR2 (100),
-  manufacturer_name VARCHAR2(100),
-  manufacture_date DATE,
-  sale_date DATE,
-  CONSTRAINT artifacts_pkey PRIMARY KEY (artifact_code)
-);
-*/
-CREATE OR REPLACE TYPE tp_artifacts AS OBJECT(
+
+CREATE OR REPLACE TYPE tp_artifact AS OBJECT(
 	artifact_code INTEGER,
-	name VARCHAR2 (100),
+	artifact_name VARCHAR2 (100),
 	manufacturer_name VARCHAR2(100),
 	manufacture_date DATE,
 	sale_date DATE
 )FINAL;
 /
-/*
-CREATE TABLE overtimes --Brazilian "hora extra"
-(
-  overtime_date DATE,
-  start_time TIMESTAMP,
-  end_time TIMESTAMP,
-  employee_cpf INTEGER NOT NULL,
-  CONSTRAINT overtimes_pkey PRIMARY KEY (overtime_date),
-  CONSTRAINT overtimes_const UNIQUE (employee_cpf),
-  CONSTRAINT overtimes_fkey FOREIGN KEY (employee_cpf) REFERENCES employees (employee_cpf)
-  
-);*/
+
 CREATE OR REPLACE TYPE tp_overtimes AS OBJECT(
 	overtime_date DATE,
 	start_time TIMESTAMP,
 	end_time TIMESTAMP,
-	employee_cpf INTEGER NOT NULL
+    ref_employee REF tp_employees
 )FINAL;
 /
-/*
 
-CREATE TABLE employee_vacancies
-(
-  employee_cpf INTEGER,
-  vacancy_number INTEGER,
-  load_unload BINARY_DOUBLE, -- 1 - Load | 0 - Unload
-  floor INTEGER,
-  CONSTRAINT employee_vacancies_pkey PRIMARY KEY (employee_cpf, vacancy_number),
-  CONSTRAINT employee_vacancies_fkey FOREIGN KEY (employee_cpf) REFERENCES employees (employee_cpf)
-);
-*/
 CREATE OR REPLACE TYPE tp_vacancies AS OBJECT(
   vacancy_number INTEGER,
   load_unload BINARY_DOUBLE, -- 1 - Load | 0 - Unload
   floor INTEGER,
-  ref_employee REF ep_employess
+  ref_employee REF tp_employees
 )FINAL;
 /
-/*CREATE TABLE sale
-(
-  employee_cpf INTEGER,
-  client_cpf INTEGER,
-  artifact_code INTEGER,
-  sale_number INTEGER,
-  date_hour TIMESTAMP,
-  CONSTRAINT sale_pkey PRIMARY KEY (employee_cpf, client_cpf, artifact_code, sale_number),
-  CONSTRAINT sale_fkey FOREIGN KEY (employee_cpf) REFERENCES employees (employee_cpf),
-  CONSTRAINT sale_fkey1 FOREIGN KEY (client_cpf) REFERENCES clients (client_cpf),
-  CONSTRAINT sale_fkey2 FOREIGN KEY (artifact_code) REFERENCES artifacts (artifact_code)
-);*/
 
--->>monitoria apenas<< estarei limitando uma venda a 4 objetos aqui para exemplificar o uso de varray
-CREATE OR REPLACE TYPE tp_artifacts_sale AS varray (4) of tp_artifacts;
+-->>monitoria apenas<< estarei limitando uma venda a 2 objetos aqui para exemplificar o uso de varray
+CREATE OR REPLACE TYPE tp_artifacts_sale AS varray (2) of tp_artifact;
 /
---3. Criação de um tipo que contenha um atributo que seja de um tipo VARRAY
+--3. CriaÃ§Ã£o de um tipo que contenha um atributo que seja de um tipo VARRAY
 CREATE OR REPLACE TYPE tp_sale AS OBJECT(
 	sale_number INTEGER,
 	date_hour TIMESTAMP,
-	ref_client REF tp_client,
-	ref_employee REF tp_employee,
-	ref_artifact REF tp_artifact_sale
+    ref_artifact tp_artifact_sale,
+	ref_client REF tp_clients,
+	ref_employee REF tp_employees
 )FINAL;
 /
 
-/*CREATE TABLE instruct
-(
-  --client_cpf INTEGER,
-  --employee_cpf INTEGER,
-  --CONSTRAINT instruct_pkey PRIMARY KEY (client_cpf, employee_cpf),
-  CONSTRAINT instruct_fkey FOREIGN KEY (client_cpf) REFERENCES clients (client_cpf),
-  CONSTRAINT instruct_fkey1 FOREIGN KEY (employee_cpf) REFERENCES employees (employee_cpf)
-);*/
 CREATE OR REPLACE TYPE tp_instruct AS OBJECT(
-	ref_client REF tp_client,
-	ref_employee REF tp_employee
+	ref_client REF tp_clients,
+	ref_employee REF tp_employees
 )FINAL;
 /
