412e870d6e86a17b25cd2ea41ab410e1ded360e1
pdssf <pdssf@cin.ufpe.br>
2017-11-16 23:10:03 -0300

    finishing create table script

--- or_sql/create_table.sql
+++ or_sql/create_table.sql
@@ -19,7 +19,8 @@ CREATE OR REPLACE TYPE tp_phones AS TABLE OF VARCHAR2(9);
 --2. CriaÃ§Ã£o de um tipo que contenha um atributo que seja de um outro tipo
 --4. CriaÃ§Ã£o de um tipo que contenha um atributo que seja de um tipo NESTED TABLE
 CREATE OR REPLACE TYPE tp_persons AS OBJECT(
-	p_name VARCHAR2(100),
+	cpf INTEGER,
+    p_name VARCHAR2(100),
 	birthdate DATE,
 	adress tp_adress,
 	phones tp_phones
@@ -39,17 +40,15 @@ ALTER TYPE tp_department ADD ATTRIBUTE (manager_cpf REF tp_persons) CASCADE;
 
  --1. CriaÃ§Ã£o de tipo e subtipo
 CREATE OR REPLACE TYPE tp_employees UNDER tp_persons(
-	cpf INTEGER,
     wage INTEGER,
   	worked_years INTEGER,
-  	job_title VARCHAR2(100),	
+  	job_title VARCHAR2(100),
   	ref_supervisor REF tp_employees,
   	ref_department_code REF tp_department
 )FINAL ;
 /
 
 CREATE OR REPLACE TYPE tp_clients UNDER tp_persons(
-	p_cpf INTEGER,
     purchases_number INTEGER,
 	heavy_guns_license BINARY_DOUBLE, -- 1 - Have the License | 0 - Don't.
   	training_end_register BINARY_DOUBLE -- 1 - Finished the training | 0 - Don't.
@@ -88,7 +87,7 @@ CREATE OR REPLACE TYPE tp_artifacts_sale AS varray (2) of tp_artifact;
 CREATE OR REPLACE TYPE tp_sale AS OBJECT(
 	sale_number INTEGER,
 	date_hour TIMESTAMP,
-    	ref_artifact tp_artifacts_sale,
+    ref_artifact tp_artifacts_sale,
 	ref_client REF tp_clients,
 	ref_employee REF tp_employees
 )FINAL;
@@ -118,25 +117,19 @@ CREATE TABLE tb_employees of tp_employees(
 	PRIMARY KEY (cpf),
 	p_name NOT NULL,
 	birthdate NOT NULL,
-	wage NOT NULL,
-	worked_years NOT NULL,
-	job_title NOT NULL,
-    sex NOT NULL,
 	FOREIGN KEY (ref_department_code) REFERENCES tb_department,
     FOREIGN KEY (ref_supervisor) REFERENCES tb_employees
-) NESTED TABLE phone STORE AS tb_phone_tec;
+)
+NESTED TABLE phones STORE AS tb_phone;
 
 --ALTER TYPE tp_department MODIFY ATTRIBUTE (manager_cpf REF tp_persons) CASCADE;
 
 CREATE TABLE tb_clients of tp_clients(
 	PRIMARY KEY (cpf),
-	p_name NOT NULL,
 	birthdate NOT NULL,
-	sex NOT NULL,
-	purchases_number NOT NULL,
-	heavy_guns_license NOT NULL,
-	training_end_register NOT NULL
-) NESTED TABLE phone STORE AS tb_phone_cli;
+	sex NOT NULL
+) 
+NESTED TABLE phones STORE AS tb_phone_cli;
 
 CREATE TABLE tb_artifact of tp_artifact(
 	PRIMARY KEY (artifact_code),
@@ -154,23 +147,17 @@ CREATE TABLE tb_overtimes of tp_overtimes(
 );
 
 CREATE TABLE tb_vacancies of tp_vacancies(
-	PRIMARY KEY(vacancy_number, ref_employee),
+	PRIMARY KEY(vacancy_number),
 	load_unload NOT NULL,
 	floor NOT NULL,
 	FOREIGN KEY (ref_employee) REFERENCES tb_employees
 );
 --0k
 CREATE TABLE tb_sale of tp_sale(
-	PRIMARY KEY(employee_cpf, client_cpf, artifact_code),
-	FOREIGN KEY(employee_cpf) REFERENCES tb_employees,
-	FOREIGN KEY(client_cpf) REFERENCES tb_clients,
-	FOREIGN KEY(artifact_code) REFERENCES tb_artifact,
-	sale_number NOT NULL,
+	PRIMARY KEY(sale_number),
+    FOREIGN KEY(ref_employee) REFERENCES tb_employees,
+	FOREIGN KEY(ref_client) REFERENCES tb_clients,
 	date_hour NOT NULL
 );
 --ok
-CREATE TABLE tb_instruct of tp_instruct(
-	PRIMARY KEY(client_cpf, employee_cpf),
-	FOREIGN KEY(client_cpf) REFERENCES tb_clients ,
-	FOREIGN KEY(employee_cpf) REFERENCES tb_employees
-);
+CREATE TABLE tb_instruct of tp_instruct;
