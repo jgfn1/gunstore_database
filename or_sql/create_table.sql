3d1e898588c96bc848523201de8cc648e96e4bc5
pdssf <pdssf@cin.ufpe.br>
2017-11-16 21:57:56 -0300

    fixing issues, employees & client table not work.

--- or_sql/create_table.sql
+++ or_sql/create_table.sql
@@ -1,4 +1,3 @@
-
 DROP TYPE tp_adress force;
 DROP TYPE tp_phones force;
 DROP TYPE tp_persons force;
@@ -14,43 +13,44 @@ CREATE OR REPLACE TYPE tp_adress AS OBJECT(
 	street varchar2(100)
 )FINAL;
 /
-CREATE OR REPLACE TYPE tp_phones AS TABLE OF VARCHAR2 (9);
+CREATE OR REPLACE TYPE tp_phones AS TABLE OF VARCHAR2(9);
 /
 
---2. Criação de um tipo que contenha um atributo que seja de um outro tipo
---4. Criação de um tipo que contenha um atributo que seja de um tipo NESTED TABLE
+--2. CriaÃ§Ã£o de um tipo que contenha um atributo que seja de um outro tipo
+--4. CriaÃ§Ã£o de um tipo que contenha um atributo que seja de um tipo NESTED TABLE
 CREATE OR REPLACE TYPE tp_persons AS OBJECT(
-	cpf INTEGER,
-	name VARCHAR2(100),
+	p_name VARCHAR2(100),
 	birthdate DATE,
 	adress tp_adress,
 	phones tp_phones
-)NOT FINAL  NOT INSTANTIABLE; -- THERE WILL BE SUBCLASSES OF tp_persons BUT THERE WILL NOT EXISTS A INSTANCE tp_persons, someone is either a employeer or a client
+)NOT FINAL NOT INSTANTIABLE; -- THERE WILL BE SUBCLASSES OF tp_persons BUT THERE WILL NOT EXISTS A INSTANCE tp_persons, someone is either a employeer or a client
 /
---14. Alteração de supertipo com propagação de mudança
-ALTER TYPE tp_persons ADD ATRIBUTE(sex VARCHAR2(1)) CASCADE;
+--14. AlteraÃ§Ã£o de supertipo com propagaÃ§Ã£o de mudanÃ§a
+ALTER TYPE tp_persons ADD ATTRIBUTE(sex VARCHAR2(1)) CASCADE;
 
 CREATE OR REPLACE TYPE tp_department AS OBJECT(
 	department_code INTEGER,
- 	name VARCHAR2 (100),
+ 	d_name VARCHAR2 (100),
  	phone_extension INTEGER --In Portuguese it is called "Ramal".
 )FINAL;
 /
+--11. AlteraÃ§Ã£o de tipo: adiÃ§Ã£o de atributo
+ALTER TYPE tp_department ADD ATTRIBUTE (manager_cpf REF tp_persons) CASCADE;
 
- --1. Criação de tipo e subtipo
+ --1. CriaÃ§Ã£o de tipo e subtipo
 CREATE OR REPLACE TYPE tp_employees UNDER tp_persons(
-	wage INTEGER,
+	cpf INTEGER,
+    wage INTEGER,
   	worked_years INTEGER,
   	job_title VARCHAR2(100),	
   	ref_supervisor REF tp_employees,
   	ref_department_code REF tp_department
 )FINAL ;
 /
---11. Alteração de tipo: adição de atributo
-ALTER TYPE tp_department ADD(manager_cpf REF tp_persons);
 
 CREATE OR REPLACE TYPE tp_clients UNDER tp_persons(
-	purchases_number INTEGER,
+	p_cpf INTEGER,
+    purchases_number INTEGER,
 	heavy_guns_license BINARY_DOUBLE, -- 1 - Have the License | 0 - Don't.
   	training_end_register BINARY_DOUBLE -- 1 - Finished the training | 0 - Don't.
 )FINAL;
@@ -84,7 +84,7 @@ CREATE OR REPLACE TYPE tp_vacancies AS OBJECT(
 -->>monitoria apenas<< estarei limitando uma venda a 2 objetos aqui para exemplificar o uso de varray
 CREATE OR REPLACE TYPE tp_artifacts_sale AS varray (2) of tp_artifact;
 /
---3. Criação de um tipo que contenha um atributo que seja de um tipo VARRAY
+--3. CriaÃ§Ã£o de um tipo que contenha um atributo que seja de um tipo VARRAY
 CREATE OR REPLACE TYPE tp_sale AS OBJECT(
 	sale_number INTEGER,
 	date_hour TIMESTAMP,
@@ -99,44 +99,51 @@ CREATE OR REPLACE TYPE tp_instruct AS OBJECT(
 	ref_employee REF tp_employees
 )FINAL;
 /
-
-ALTER TYPE tp_department DROP ATRIBUTE (manager_cpf REF tp_persons) CASCADE; 
+DROP TABLE tb_department CASCADE CONSTRAINTS;
+DROP TABLE tb_employees CASCADE CONSTRAINTS;
+DROP TABLE tb_clients CASCADE CONSTRAINTS;
+DROP TABLE tb_artifact CASCADE CONSTRAINTS;
+DROP TABLE tb_overtimes;
+DROP TABLE tb_vacancies;
+DROP TABLE tb_sale;
+DROP TABLE tb_instruct;
 
 CREATE TABLE tb_department of tp_department(
 	PRIMARY KEY (department_code),
-	name NOT NULL,
+	d_name NOT NULL,
 	phone_extension NOT NULL
 );
 
 CREATE TABLE tb_employees of tp_employees(
 	PRIMARY KEY (cpf),
-	name NOT NULL,
+	p_name NOT NULL,
 	birthdate NOT NULL,
-	sex NOT NULL,
 	wage NOT NULL,
 	worked_years NOT NULL,
 	job_title NOT NULL,
-	FOREIGN KEY (ref_department_code) REFERENCES tb_department
-)NESTED TABLE phone AS tb_phone_tec;
+    sex NOT NULL,
+	FOREIGN KEY (ref_department_code) REFERENCES tb_department,
+    FOREIGN KEY (ref_supervisor) REFERENCES tb_employees
+) NESTED TABLE phone STORE AS tb_phone_tec;
 
-ALTER TYPE tp_department MODIFY ATRIBUTE (manager_cpf REF tp_persons) CASCADE;
+--ALTER TYPE tp_department MODIFY ATTRIBUTE (manager_cpf REF tp_persons) CASCADE;
 
 CREATE TABLE tb_clients of tp_clients(
 	PRIMARY KEY (cpf),
-	name NOT NULL,
+	p_name NOT NULL,
 	birthdate NOT NULL,
 	sex NOT NULL,
 	purchases_number NOT NULL,
 	heavy_guns_license NOT NULL,
 	training_end_register NOT NULL
-)NESTED TABLE phone AS tb_phone_cli;
+) NESTED TABLE phone STORE AS tb_phone_cli;
 
 CREATE TABLE tb_artifact of tp_artifact(
 	PRIMARY KEY (artifact_code),
 	artifact_name NOT NULL,
-	 manufacturer_name NOT NULL,
-	 manufacture_date NOT NULL,
-	 sale_date
+    manufacturer_name NOT NULL,
+    manufacture_date NOT NULL,
+	sale_date NOT NULL
 );
 
 CREATE TABLE tb_overtimes of tp_overtimes(
